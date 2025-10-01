512d22389459c84e26fbc53563a1a3227c63d732
NisidhKumar <kumarnisdh@gmail.com>
2025-10-01 21:23:40 +0530

    deleteLastNode solved

--- DSA/LinkedList/deleteLastNode.cpp
+++ DSA/LinkedList/deleteLastNode.cpp
@@ -1,14 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-int fun(){
+struct Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+Node *deleteLast(Node *head){
+    if (head == NULL){
+        return NULL;
+    }
+    if (head->next==NULL){
+        delete head;
+        return NULL;
+    }
+    Node *curr= head;
+    while(curr->next->next!=NULL){
+        curr = curr->next;
+    }
+     delete curr->next;
+     curr->next = NULL;
+       return head;
+ 
 
+}
+void printlist(Node *head){
+    while(head!=NULL){
+        cout<<head->data<< " ";
+        head= head->next;
+    }
+    cout<< "\n";
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    
+    Node *head = new Node (1001);
+    head->next = new Node (1085);
+    head->next->next = new Node (1585);
+    cout << "without deltion\n";
+    printlist(head);
+    head = deleteLast(head);
+    cout << "With deletion\n";
+    printlist(head);
     
     return 0;
 }
