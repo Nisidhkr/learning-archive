bad6d6b12be9d67d04cb9f2bdbd51694f564ed7f
NisidhKumar <kumarnisdh@gmail.com>
2025-10-05 14:17:37 +0530

    learnt some more concepts on doubly linkedlist

---
+++ DSA/Stack/stackImplementationLInkedList.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node next;
+    Node(int x){
+        
+    }
+    /* data */
+};
+
+struct myStack
+{
+    int top;
+    int *arr;
+    int capacity;
+    /* data */
+};
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+    
+    return 0;
+}
